c331a3c0aea5350667a9b79ca48f92598361b0d4
yuki sakura <yukisakura1993@gmail.com>
2023-11-01 17:25:17 +0900

    fix

--- recorder.py
+++ recorder.py
@@ -1,34 +1,38 @@
 import cv2
 from pathlib import Path
 
-RECORD_TIME = 3600
+output_root = Path("outputs")
+output_root.mkdir(exist_ok=True, parents=True)
+
+RECORD_TIME = 5
 FPS = 30
-def recording(name):
-    output_root = Path("outputs")
-    output_root.mkdir(exist_ok=True, parents=True)
+def recording(cap, video_writer):
+    try:
+        loop = int(FPS * RECORD_TIME)
+        for i in range(loop):
+            _, frame = cap.read()
+            video_writer.write(frame)
+    except Exception:
+        video_writer.release()
+        cap.release()
+    video_writer.release()
+        
+def main():
+    index = 0
     cap = cv2.VideoCapture(0)
-    fps = FPS
-    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    video = cv2.VideoWriter(str(output_root / name), fourcc, fps, (w,h))
- 
-    print(f"recording start {name}")
-    loop = int(fps * RECORD_TIME)
-    for i in range(loop):
-        _, frame = cap.read()
-        video.write(frame)
-    print(f"recording stop {name}")
-    video.release()
-    cap.release()
-    return video
-
-index = 0
-is_recording = True
-while is_recording:
-    try:
-        recording(f"recording_{index}.mp4")
+    while True:
+        fps = FPS
+        w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        name = f"recording_{index}.mp4"
+        video_writer = cv2.VideoWriter(str(output_root / name), fourcc, fps, (w,h))
+        
+        print(f"start {name}")
+        recording(cap, video_writer)
+        print(f"stop {name}")
         index += 1
-    except Exception:
-        is_recording = False
+    
+if __name__ == "__main__":
+    main()
